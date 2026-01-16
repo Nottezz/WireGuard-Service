20cb649aff318c82b2a21ac54e1dad668117bd9e
Nikita Yakovlev <driftworks2013@yandex.ru>
2026-01-16 18:46:34 +0300

    include new router - generate key

--- wireguard_service/api/__init__.py
+++ wireguard_service/api/__init__.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
 from .views.config_view import router as show_view
+from .views.generate_key import router as generate_key
 
 router = APIRouter(
     prefix="/api",
 )
 router.include_router(show_view)
+router.include_router(generate_key)
