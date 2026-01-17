ec08ec0fd61e8be5ad9804777c59f842299bf412
Nikita Yakovlev <driftworks2013@yandex.ru>
2026-01-17 16:07:42 +0300

    rename show schemas to interface

--- wireguard_service/schemas/interface.py
+++ wireguard_service/schemas/interface.py
@@ -1,18 +1,17 @@
 from typing import List, Optional
 from pydantic import BaseModel
 
-
-class Peer(BaseModel):
-    public_key: str
-    endpoint: Optional[str] = None
-    allowed_ips: Optional[str] = None
-    latest_handshake: Optional[str] = None
-    transfer: Optional[str] = None
+from schemas.peer import Peer
 
 
-class Interface(BaseModel):
+class InterfaceBase(BaseModel):
     name: str
     public_key: Optional[str] = None
     private_key: Optional[str] = None
     listening_port: Optional[int] = None
     peers: List[Peer] = []
+
+class Interface(InterfaceBase):
+    """
+    Base model for all interfaces.
+    """
