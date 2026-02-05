e31dd1478f9db40d8f4cba46a74622f86557c04e
Nikita Yakovlev <driftworks2013@yandex.ru>
2026-02-05 15:01:47 +0300

    add base model

---
+++ wireguard_service/models/base.py
@@ -0,0 +1,16 @@
+from sqlalchemy import MetaData
+from sqlalchemy.orm import DeclarativeBase, declared_attr
+
+from wireguard_service.config import settings
+from wireguard_service.helpers import camel_case_to_snake_case
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    metadata = MetaData(
+        naming_convention=settings.database.naming_convention
+    )
+
+    @declared_attr.directive
+    def __tablename__(self) -> str:
+        return f"{camel_case_to_snake_case(self.__class__.__name__)}s"
