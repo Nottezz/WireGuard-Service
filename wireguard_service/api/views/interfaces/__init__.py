51afdbab72f5cc97f0263345ce92103d39c3609f
Nikita Yakovlev <driftworks2013@yandex.ru>
2026-02-05 22:15:07 +0300

    remove old endpoints

--- wireguard_service/api/views/interfaces/__init__.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-from .config_view import router as config_router
-
-router = APIRouter(
-    prefix="/interfaces/{interface}",
-    tags=["Interfaces"],
-)
-router.include_router(config_router)
