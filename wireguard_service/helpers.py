a9ef9e5ea8cbc3678d602119dbdef312fe2ad6db
Nikita Yakovlev <driftworks2013@yandex.ru>
2026-02-05 15:03:01 +0300

    add helpers method for table name

--- wireguard_service/helpers.py
+++ wireguard_service/helpers.py
@@ -44,3 +44,30 @@ def parse_wg_show(output: str) -> Interface:
         peers.append(current_peer)
 
     return Interface(peers=[Peer(**p) for p in peers], **iface_data)
+
+def camel_case_to_snake_case(input_str: str) -> str:
+    """
+    >>> camel_case_to_snake_case("SomeSDK")
+    'some_sdk'
+    >>> camel_case_to_snake_case("RServoDrive")
+    'r_servo_drive'
+    >>> camel_case_to_snake_case("SDKDemo")
+    'sdk_demo'
+
+    Taken from
+    https://github.com/mahenzon/ri-sdk-python-wrapper/blob/master/ri_sdk_codegen/utils/case_converter.py
+    """
+    chars = []
+    for c_idx, char in enumerate(input_str):
+        if c_idx and char.isupper():
+            nxt_idx = c_idx + 1
+            # idea of the flag is to separate abbreviations
+            # as new words, show them in lower case
+            flag = nxt_idx >= len(input_str) or input_str[nxt_idx].isupper()
+            prev_char = input_str[c_idx - 1]
+            if prev_char.isupper() and flag:
+                pass
+            else:
+                chars.append("_")
+        chars.append(char.lower())
+    return "".join(chars)
