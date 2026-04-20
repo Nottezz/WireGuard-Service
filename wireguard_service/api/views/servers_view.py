8242b777b619e608455f2cd45d9a4ff1f6244645
Nikita Yakovlev <driftworks2013@yandex.ru>
2026-04-20 20:35:57 +0300

    router fix

--- wireguard_service/api/views/servers_view.py
+++ wireguard_service/api/views/servers_view.py
@@ -6,7 +6,7 @@ from wireguard_service.storages import servers
 from ..dependencies import DateBaseDepends, WGClientDepends
 
 router = APIRouter(
-    prefix="/server/{server_name}",
+    prefix="/server",
     tags=["Servers"],
 )
 
@@ -16,12 +16,12 @@ def get_servers(db: DateBaseDepends) -> list[ServerRead]:
     return servers.get_list_servers(db)
 
 
-@router.get("/")
+@router.get("/{server_name}")
 def get_server(db: DateBaseDepends, server_name: str) -> ServerRead:
     return servers.get_server(db, server_name)
 
 
-@router.get("/check")
+@router.get("/{server_name}/check")
 def check_server_connection(
     db: DateBaseDepends, server_name: str, wg_client: WGClientDepends
 ) -> bool:
@@ -29,18 +29,18 @@ def check_server_connection(
     return wg_client.check_connection(str(server.host), server.port, server.username)
 
 
-@router.post("/")
+@router.post("/{server_name}")
 def create_server(db: DateBaseDepends, server_in: ServerCreate) -> ServerRead:
     return servers.add_server(db, server_in)
 
 
-@router.patch("/")
+@router.patch("/{server_name}")
 def partial_update_server(
     db: DateBaseDepends, server_in: ServerUpdate, server_name: str
 ) -> ServerRead:
     return servers.partial_update_server_info(db, server_in, server_name=server_name)
 
 
-@router.delete("/")
+@router.delete("/{server_name}")
 def delete_server(db: DateBaseDepends, server_name: str) -> ServerRead:
     return servers.delete_server(db, server_name)
